1bd3780fdf79915dd26b7e3e35a0f2849137fd82
上島綺夏 <a8119022@gsuite.si.aoyama.jp>
2021-05-25 19:11:44 +0900

    prac1-5

--- aandd.js
+++ aandd.js
@@ -10,3 +10,20 @@ function show(array) {
     //document.getElementById('res').innerHTML = html;
     document.getElementById('res').insertAdjacentHTML('beforeend', html);
 }
+
+function createSequentialNumbers(array, n){
+    var x = 1;
+    for(var j = 0; j < n; j++){
+        array.push(x);
+        console.log(array[x]);
+        x = x + 1;
+    }
+}
+
+function createRandomNumbers(array, n){
+    for(var i = 0; i < n; i++){
+         var x = Math.round(Math.random()*100);
+         array.push(x);
+         console.log(array[x]);
+    }
+}
